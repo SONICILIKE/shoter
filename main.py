c6ca0c936c040a542c5ed85428ff42abb4aa0208
Nikita_Moshonkin <145210277+SONICILIKE@users.noreply.github.com>
2026-09-19 15:27:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from kivy.core.window import Window
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivy import platform
+
+class MainScreen(MDScreen):
+    ...
+
+class GameScreen(MDScreen):
+    ...
+
+class ShooterApp(MDApp):
+    def build(self):
+    self.theme_cls.theme_style ="Dark"
+    self.theme_cls.primary_pallet = "Gray"
+
+    self.sm = MDScreenManager()
+
+    self.sm.add_widget(MainScreen(name="main"))
+    self.sm.add_widget(MainScreen(name="game"))
+
+    return self.sm
+
+if platform != "android":
+    Window.size = (450,900)
+    Window.top = 100
+    Window.left = 600
+
+app = ShooterApp()
+app.run()
