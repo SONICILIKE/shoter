ac004586fa7f3a52c3d68a5162f8cb029c1ab249
Nikita_Moshonkin <145210277+SONICILIKE@users.noreply.github.com>
2026-09-19 15:29:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ class GameScreen(MDScreen):
 
 class ShooterApp(MDApp):
     def build(self):
-    self.theme_cls.theme_style ="Dark"
-    self.theme_cls.primary_pallet = "Gray"
+        self.theme_cls.theme_style ="Dark"
+        self.theme_cls.primary_pallet = "Gray"
 
-    self.sm = MDScreenManager()
+        self.sm = MDScreenManager()
 
-    self.sm.add_widget(MainScreen(name="main"))
-    self.sm.add_widget(MainScreen(name="game"))
+        self.sm.add_widget(MainScreen(name="main"))
+        self.sm.add_widget(MainScreen(name="game"))
 
-    return self.sm
+        return self.sm
 
 if platform != "android":
     Window.size = (450,900)
